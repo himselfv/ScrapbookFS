6974826d56ebd41a298e31e9ccb687f69bf94b11
himselfv <none@none>
2016-04-01 05:03:26 -0700

    Fix: use WinRAR GUI version to pack to zip.

--- compile.cmd
+++ compile.cmd
@@ -4,6 +4,6 @@ set PATH=C:\Program Files\WinRAR;%PATH%
 del scrapbookx.xpi
 rem so that it's not archived
 
-rar a -r -x@compile.exclude.txt scrapbookx.zip *
-rar a -r -n@compile.include.txt scrapbookx.zip *
+winrar a -afzip -r -x@compile.exclude.txt scrapbookx.zip *
+winrar a -afzip -r -n@compile.include.txt scrapbookx.zip *
 ren scrapbookx.zip scrapbookx.xpi
